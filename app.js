91c1b236c0144570e5a2dfe4c96f089425fa17f6
ercain03 <ercain3@gmail.com>
2016-04-13 23:42:29 -0700

    Added a number tally of the amount of questions the user gets correct and console.logs to the questions for debugging.

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+
 var userName = prompt('Hello friend, what is your name?');
 console.log('The users name is ' + userName);
 
 alert('What\'s up ' + userName + '? Let\'s play a guessing game about me! please answer yes or no to the following questions.');
 
+var numberTally = 0;
+
 var userQuestions = ['Do I have a brother?', userName + ' , Do I have a belly button?', userName + ' , Do I love ice cream?', userName + ' , is Los Angeles my favorite city?', userName + ' , Is purple my favorite color?'];
 
 for (var i = 0; i < userQuestions.length; i++) {
   var userResponse = prompt(userQuestions[i]);
+  console.log('The users response is: ' + userResponse);
   if (userResponse.toLowerCase() === 'y' || userResponse.toUpperCase() === 'YES') {
     alert('Right on ' + userName + '! :)');
+    numberTally += 1;
   } else if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
     alert('Try Again ' + userName + '! :/');
   } else {
@@ -19,7 +24,7 @@ for (var i = 0; i < userQuestions.length; i++) {
 alert('Okay, see now that wasn\'t to hard. But you\'re not finished yet.' );
 
 var funFactor = prompt('Are you having fun yet? yes or no?');
-console.log('The user is answer is ' + funFactor);
+console.log('The users answer is ' + funFactor);
 
 if (funFactor.toLowerCase() === 'y' || funFactor.toUpperCase() === 'YES') {
   alert('Great! ' + userName + ' I\'m so glad you\'re enjoying yourself.');
@@ -32,8 +37,10 @@ if (funFactor.toLowerCase() === 'y' || funFactor.toUpperCase() === 'YES') {
 var i = 0;
 while (i <= 3) {
   var userQuestion2 = parseInt(prompt('What is my favorite number? Hint: it\'s a number between 1-10.'));
+  console.log('The users favorite answer is ' + userQuestion2);
   if (userQuestion2 === 3){
     alert(userName + ' , you got it right!');
+    numberTally += 1;
     break;
   } else if (userQuestion2 > 3) {
     alert(userName + ' , your guess is to high.');
@@ -48,6 +55,7 @@ while (i <= 3) {
 alert('Now, for your final question.');
 
 var finalQuestion = prompt('Are you ready?');
+console.log('The users response is: ' + finalQuestion);
 if (funFactor.toLowerCase() === 'y' || funFactor.toUpperCase() === 'YES') {
   alert('Drum roll please!!!!');
 } else if (funFactor.toLowerCase() === 'n' || funFactor.toUpperCase() === 'NO') {
@@ -60,14 +68,18 @@ var myEthnicity = ['AFRICAN-AMERICAN', 'PUERTO RICAN', 'GERMAN'];
 var i = 0;
 while (i <= 5) {
   var userQuestion3 = prompt('Can you guess what ethnicity I am?');
+  console.log('The users response is: ' + userQuestion3);
   if (userQuestion3.toUpperCase() === myEthnicity[0]){
     alert('you got it right.');
+    numberTally += 1;
     break;
   } else if (userQuestion3.toUpperCase() === myEthnicity[1]){
     alert('you got it right.');
+    numberTally += 1;
     break;
   } else if (userQuestion3.toUpperCase() === myEthnicity[2]){
     alert('you got it right.');
+    numberTally += 1;
     break;
   } else {
     alert('please try again.');
@@ -76,3 +88,9 @@ while (i <= 5) {
 }
 
 alert('I am racially mixed with ' + myEthnicity[0] + ', ' + myEthnicity[1] + ', ' + myEthnicity[2]);
+
+if (numberTally >= 5){
+  alert('Good job, ' + userName + ' your results are ' + numberTally + ' out of 7 questions correct. You know me pretty well.');
+} else {
+  alert('Your results are ' + numberTally + ' out of 7 questions, you don\'t know me at all!!');
+}
